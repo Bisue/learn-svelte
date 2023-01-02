117ea09a37606ed899d2956c6408ba234941f245
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-01-02 22:20:03 +0900

    tiny counter component example

---
+++ src/components/Counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+let count = 0;
+
+function increase() {
+    count++;
+}
+
+function decrease() {
+    count--;
+}
+</script>
+
+<div>
+    <span>{count}</span>
+    <button on:click={increase}>Up</button>
+    <button on:click={decrease}>Down</button>
+</div>
