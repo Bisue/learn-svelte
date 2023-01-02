3e403185a59b4ac0aa4b0adfd7a29ee52db78b65
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-01-02 22:26:03 +0900

    component styling (like vue SFC)

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,17 +1,39 @@
 <script lang="ts">
-let count = 0;
+  let count = 0;
 
-function increase() {
+  function increase() {
     count++;
-}
+  }
 
-function decrease() {
+  function decrease() {
     count--;
-}
+  }
 </script>
 
 <div>
-    <span>{count}</span>
-    <button on:click={increase}>Up</button>
-    <button on:click={decrease}>Down</button>
+  <span class="count">{count}</span>
+  <button on:click={increase}>Up</button>
+  <button on:click={decrease}>Down</button>
 </div>
+
+<style>
+  .count {
+    font-weight: bold;
+  }
+
+  button {
+    background: none;
+    outline: none;
+    border: none;
+    border-radius: 3px;
+    border: 1px solid #afafaf;
+    background: #ededed;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    transition: all 0.25s;
+  }
+
+  button:hover {
+    background: rgb(219, 219, 219);
+  }
+</style>
