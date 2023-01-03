755ff818eef326f8951fc466b48e5244c3ef4f47
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-01-03 17:33:22 +0900

    each template block

---
+++ src/components/List.svelte
@@ -0,0 +1,15 @@
+<script>
+  let lists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+</script>
+
+<ul>
+  {#each lists as list}
+    <li>{list}</li>
+  {/each}
+</ul>
+
+<!-- <ul>
+  {#each lists as list, idx}
+    <li>{list}</li>
+  {/each}
+</ul> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-    import Counter from "../components/Counter.svelte";
+  import Counter from '../components/Counter.svelte';
+  import List from '../components/List.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Counter/>
+<Counter />
+<List />
