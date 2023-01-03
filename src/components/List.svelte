edb637d7a9bb3ebb9467338531e8ffff07cd34ac
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-01-03 17:34:30 +0900

    keying each template block (important)

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -2,14 +2,20 @@
   let lists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 </script>
 
-<ul>
+<!-- <ul>
   {#each lists as list}
     <li>{list}</li>
   {/each}
-</ul>
+</ul> -->
 
 <!-- <ul>
   {#each lists as list, idx}
     <li>{list}</li>
   {/each}
 </ul> -->
+
+<ul>
+  {#each lists as list (list)}
+    <li>{list}</li>
+  {/each}
+</ul>
