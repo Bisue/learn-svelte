426c1771a3e7133992f9b09b8241a442ba118a0b
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-01-04 23:07:34 +0900

    event modifier

--- src/routes/sub/+page.svelte
+++ src/routes/sub/+page.svelte
@@ -1,9 +1,12 @@
 <script>
   import Prop from '../../components/Prop.svelte';
   import Event from '../../components/Event.svelte';
+  import OnlyOne from '../../components/OnlyOne.svelte';
 </script>
 
 <Prop message={'Greeting'}/>
 <Prop/>
 
 <Event/>
+
+<OnlyOne/>
