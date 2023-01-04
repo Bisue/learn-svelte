ca32468e9eac81b3fae5e6d92aba7acb7a0efdb7
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-01-04 23:06:36 +0900

    dom event handling

---
+++ src/components/Event.svelte
@@ -0,0 +1,20 @@
+<script>
+  let m = { x: 0, y: 0 };
+  
+  function handleMousemove(event) {
+    m.x = event.clientX;
+	m.y = event.clientY;
+  }
+</script>
+
+<div on:mousemove={handleMousemove}>
+	The mouse position is {m.x} x {m.y}
+</div>
+
+<style>
+  div {
+    width: 250px;
+    height: 250px;
+    border: 1px solid lightgray;
+  }
+</style>
--- src/routes/sub/+page.svelte
+++ src/routes/sub/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   import Prop from '../../components/Prop.svelte';
+  import Event from '../../components/Event.svelte';
 </script>
 
 <Prop message={'Greeting'}/>
 <Prop/>
+
+<Event/>
